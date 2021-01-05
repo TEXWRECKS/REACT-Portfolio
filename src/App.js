5dcd62b69553294f2a3c0b61d87186051415596a
Greg Malone <atx76dragon@yahoo.com>
2021-01-05 13:26:02 -0600

    Added Navbar.js to Extras folder.  Added Projects.js to Components folder.  Updated App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Navbar from './Extras/Navbar';
-import AboutMe from './Pages/AboutMe.js';
+import AboutMe from './Pages/AboutMe';
 import MyProjects from './Pages/MyProjects';
 import Resume from './Pages/Resume';
 import ContactMe from './Pages/ContactMe';
